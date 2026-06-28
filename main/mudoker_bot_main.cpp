45620ef5f24c23c811a04f3119d5f3d0ef158bb7
mudoker <huuquoc7603@gmail.com>
2026-06-28 22:48:12 +0700

    ref: update logic and resolve bugs

--- main/mudoker_bot_main.cpp
+++ main/mudoker_bot_main.cpp
@@ -1,11 +1,19 @@
+#include "assets/faces.hpp"
 #include "hal/i2c_types.h"
 #include "i2c/i2c.hpp"
+#include "screen/screen.hpp"
 #include "soc/clk_tree_defs.h"
 #include "soc/gpio_num.h"
 
-void app_main(void) {
-  I2CManager i2c_manager;
+i2c_master_bus_handle_t i2c_bus_handler = nullptr;
 
-  i2c_manager.init_i2c_master_bus(I2C_CLK_SRC_DEFAULT, I2C_NUM_0, GPIO_NUM_4,
-                                  GPIO_NUM_5, true);
+extern "C" void app_main(void) {
+  I2CManager i2c_manager;
+  SSD1306Screen ssd13068_screen;
+
+  i2c_manager.init_i2c_master_bus(I2C_CLK_SRC_DEFAULT, I2C_NUM_0, GPIO_NUM_5,
+                                  GPIO_NUM_4, true);
+
+  ssd13068_screen.init_lcd_panel(i2c_bus_handler);
+  ssd13068_screen.render_bitmap(0, 0, EXCITED_FACE);
 }
