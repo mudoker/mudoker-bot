75dec8edd8b2a77a80e95e34d1bad8517bdb6e3f
mudoker <huuquoc7603@gmail.com>
2026-07-01 14:20:11 +0700

    refactor: reorganize code structure and improve servo management integration

--- main/mudoker_bot_main.cpp
+++ main/mudoker_bot_main.cpp
@@ -1,27 +1,54 @@
 #include "assets/faces.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "hal/i2c_types.h"
 #include "i2c/i2c.hpp"
 #include "screen/screen.hpp"
+#include "servo/servo.hpp"
 #include "soc/clk_tree_defs.h"
 #include "soc/gpio_num.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 i2c_master_bus_handle_t i2c_bus_handler = nullptr;
+I2CManager i2c_manager;
+SSD1306Screen ssd13068_screen;
+ServoManager servo_manager;
+
+void show_face(int *face_index);
+void set_servo_angle(int *servo_index, int *angle);
 
 extern "C" void app_main(void) {
-  I2CManager i2c_manager;
-  SSD1306Screen ssd13068_screen;
-
   i2c_manager.init_i2c_master_bus(I2C_CLK_SRC_DEFAULT, I2C_NUM_0, GPIO_NUM_5,
                                   GPIO_NUM_4, true);
 
   ssd13068_screen.init_lcd_panel(i2c_bus_handler);
 
-  int face_index = 0;
-  while (true) {
-    ssd13068_screen.render_bitmap(0, 0, ALL_FACES[face_index]);
-    face_index = (face_index + 1) % FACE_COUNT;
-    vTaskDelay(pdMS_TO_TICKS(500));
+  servo_manager.init_servo_timer(0, MCPWM_TIMER_CLK_SRC_DEFAULT, 1000000, 20000, MCPWM_TIMER_COUNT_MODE_UP);
+  for (int i = 0; i < sizeof(servo_pins) / sizeof(servo_pins[0]); i++) {
+    servo_manager.attach_servo(0, i);
   }
+
+  int face_index = 0;
+  int servo_angle = 0;
+  int servo_index = 0;
+  while (true) {
+    show_face(&face_index);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    set_servo_angle(&servo_index, &servo_angle);
+  }
+}
+
+void show_face(int *face_index) {
+  if (*face_index < 0 || *face_index >= FACE_COUNT) {
+    ESP_LOGE("MAIN", "Invalid face index: %d", *face_index);
+    return;
+  }
+
+  ssd13068_screen.render_bitmap(0, 0, ALL_FACES[*face_index]);
+  *face_index = (*face_index + 1) % FACE_COUNT;
+}
+
+void set_servo_angle(int *servo_index, int *angle) {
+  servo_manager.set_servo_angle(*servo_index, *angle);
+  *servo_index = (*servo_index + 1) % SERVO_CNT;
+  *angle = (*angle + 30) % 180;
 }
