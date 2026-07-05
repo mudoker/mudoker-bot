d0748d6f8bf705746b71eb19c767bc1f59087ebb
mudoker <huuquoc7603@gmail.com>
2026-07-05 11:50:39 +0700

    fix: update servo count to 8 and refactor timer configuration parameters

--- main/servo/servo.hpp
+++ main/servo/servo.hpp
@@ -12,7 +12,8 @@
 #define PULSE_WIDTH_RANGE (SERVO_MAX_PULSE_WIDTH - SERVO_MIN_PULSE_WIDTH)
 #define PULSE_WIDTH_BUFFER 100
 #define SERVO_MAX_DEGREE 180
-#define SERVO_CNT 1
+#define SERVO_CNT 8
+#define MICROSECONDS_PER_SECOND 1000000
 
 extern int servo_pins[SERVO_CNT];
 
@@ -20,6 +21,11 @@ int convert_degree_to_pulse_width(int degree);
 
 class ServoManager {
 public:
+  ledc_mode_t speed_mode = LEDC_LOW_SPEED_MODE;
+  ledc_timer_t timer_sel = LEDC_TIMER_0;
+  ledc_timer_bit_t duty_resolution = LEDC_TIMER_13_BIT;
+  uint32_t freq_hz = 50;
+
   virtual ~ServoManager() = default;
   void set_servo_angle(int servo_index, int angle);
 
@@ -27,10 +33,10 @@ public:
     ESP_LOGI("SERVO", "Initialising LEDC Timer for Servo...");
 
     ledc_timer_config_t ledc_timer = {};
-    ledc_timer.speed_mode       = LEDC_LOW_SPEED_MODE;
-    ledc_timer.duty_resolution  = LEDC_TIMER_13_BIT;
-    ledc_timer.timer_num        = LEDC_TIMER_0;
-    ledc_timer.freq_hz          = 50;
+    ledc_timer.speed_mode       = speed_mode;
+    ledc_timer.duty_resolution  = duty_resolution;
+    ledc_timer.timer_num        = timer_sel;
+    ledc_timer.freq_hz          = freq_hz;
     ledc_timer.clk_cfg          = LEDC_USE_APB_CLK;
 
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
@@ -47,9 +53,9 @@ public:
 
     ledc_channel_config_t ledc_channel = {};
     ledc_channel.gpio_num       = servo_pins[servo_index];
-    ledc_channel.speed_mode     = LEDC_LOW_SPEED_MODE;
+    ledc_channel.speed_mode     = speed_mode;
     ledc_channel.channel        = (ledc_channel_t)servo_index;
-    ledc_channel.timer_sel      = LEDC_TIMER_0;
+    ledc_channel.timer_sel      = timer_sel;
     ledc_channel.duty           = 0;
     ledc_channel.hpoint         = 0;
 
